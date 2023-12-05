da868306dce9cfced2d58d32711957c4cf595bcf
Alessandro Palermo <alessandro.palermo@mobilesoft.it>
2023-12-05 16:32:18 +0100

    Fixed test for SingleApiCallCubit, updated min Flutter version to 3.10.0

--- test/src/logic/cubit/single_api_call/single_api_call_cubit_test.dart
+++ test/src/logic/cubit/single_api_call/single_api_call_cubit_test.dart
@@ -5,23 +5,49 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
-  blocTest<SingleApiCallCubit, SingleApiCallState>(
-    'emits [ApiCallLoadingState] and [ApiCallLoadedState] when startApiCall is called with success.',
-    build: () => SingleApiCallCubit(
-        apiCall: () =>
-            ConnectionManagerStub().doApiRequest(endpoint: "mocks/test.json")),
-    act: (cubit) => cubit.startApiCall(),
-    expect: () => [ApiCallLoadingState(), isA<ApiCallLoadedState>()],
-  );
+  test(
+      "emits [ApiCallLoadingState] and [ApiCallLoadedState] at init and when startApiCall is called with success.",
+      () async {
+    var future =
+        ConnectionManagerStub().doApiRequest(endpoint: "mocks/test.json");
 
-  blocTest<SingleApiCallCubit, SingleApiCallState>(
-    'emits [ApiCallLoadingState] and [ApiCallErrorState] when startApiCall is called with error.',
-    build: () => SingleApiCallCubit(
-        apiCall: () => ConnectionManagerStub(responseStatusCode: 500)
-            .doApiRequest(endpoint: "mocks/test.json")),
-    act: (cubit) => cubit.startApiCall(),
-    expect: () => [ApiCallLoadingState(), isA<ApiCallErrorState>()],
-  );
+    final cubit = SingleApiCallCubit(apiCall: () => future);
+
+    expect(cubit.state, ApiCallLoadingState());
+
+    expectLater(cubit.stream, emitsInOrder([isA<ApiCallLoadedState>()]));
+
+    await future;
+
+    expectLater(
+      cubit.stream,
+      emitsInOrder([ApiCallLoadingState(), isA<ApiCallLoadedState>()]),
+    );
+
+    cubit.startApiCall();
+  });
+
+  test(
+      "emits [ApiCallLoadingState] and [ApiCallErrorState] at init and when startApiCall is called with error.",
+      () async {
+    var future = ConnectionManagerStub(responseStatusCode: 500)
+        .doApiRequest(endpoint: "mocks/test.json");
+
+    final cubit = SingleApiCallCubit(apiCall: () => future);
+
+    expect(cubit.state, ApiCallLoadingState());
+
+    expectLater(cubit.stream, emitsInOrder([isA<ApiCallErrorState>()]));
+
+    await future;
+
+    expectLater(
+      cubit.stream,
+      emitsInOrder([ApiCallLoadingState(), isA<ApiCallErrorState>()]),
+    );
+
+    cubit.startApiCall();
+  });
 
   blocTest<SingleApiCallCubit, SingleApiCallState>(
     'verify `response` is correct after startApiCall is called with success.',
